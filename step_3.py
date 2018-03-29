50309707d784724ffee7f6259909208f980fbeca
Xavier <nomad0x@gmail.com>
2018-03-29 05:48:21 +0000

    completed up to step 3

--- step_3.py
+++ step_3.py
@@ -2,4 +2,12 @@ from step_1 import transform_products_to_list
 
 
 def group_products_by_customer_and_invoice(products_string):
-    pass
+    products_list = transform_products_to_list(products_string)
+    customers = {}
+    for product in products_list:
+        invoice_id = product[0]
+        customer_id = product[-2]
+        customers.setdefault(customer_id, {})
+        customers[customer_id].setdefault(invoice_id, [])
+        customers[customer_id][invoice_id].append(product)
+    return customers
